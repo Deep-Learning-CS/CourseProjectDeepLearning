4f42ebc3158977cfb64a7cf75409dbf12c63da46
Sanidhaya <Sanidhaya08sharma11@gmail.com>
2024-10-29 22:44:02 -0400

    changes in main file

--- Deployment/main.py
+++ Deployment/main.py
@@ -1,8 +1,10 @@
 from io import BytesIO
 
+import torch
+import torchaudio
+from df import enhance, init_df
 from fastapi import FastAPI, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-from pydub import AudioSegment
 from starlette.responses import StreamingResponse
 
 app = FastAPI()
@@ -15,24 +17,26 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+model, df_state, _ = init_df()
+
 @app.put("/process-audio/", response_class=StreamingResponse)
 async def process_audio(file: UploadFile):
     try:
-        audio = AudioSegment.from_file(file.file)
-        
-        processed_audio = audio + 5
-        
+        audio_tensor, sample_rate = torchaudio.load(BytesIO(await file.read()))
+
+        enhanced_audio_tensor = enhance(model, df_state, audio_tensor)
+
         buffer = BytesIO()
-        processed_audio.export(buffer, format="wav")
+        torchaudio.save(buffer, enhanced_audio_tensor, sample_rate, format="wav")
         buffer.seek(0)
 
-        return StreamingResponse(buffer, media_type="audio/wav", headers={"Content-Disposition": "attachment; filename=processed_audio.wav"})
+        return StreamingResponse(buffer, media_type="audio/wav", headers={
+            "Content-Disposition": "attachment; filename=processed_audio.wav"
+        })
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-
 @app.get("/")
 async def root():
     return {"message": "Hello, Professor!"}
-
